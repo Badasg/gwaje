99d06ba2732ea1867d32e671cea6b32e11db6aba
BadaSg <gwajeyong0621@naver.com>
2023-04-11 14:30:14 +0900

    duck

--- print.c
+++ print.c
@@ -3,7 +3,7 @@
 int main()
 {
 	printf("koalas are so cute.\n");
-	printf("Ducks never feel cold, even if they swim in cold water.\n");
+	printf("Ducks are relatives of geese and swans.\n");
 	printf("Platypuses are not very comfortable on land.\n");
 	printf("Bears have a very good sense of smell.\n");
 	printf("Pandas are very special animals.\n");
