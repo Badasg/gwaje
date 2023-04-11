e7d77fbed78b9337b3bd58a3483d6c688a62f5fb
BadaSg <gwajeyong0621@naver.com>
2023-04-11 14:22:13 +0900

    과제 1

---
+++ print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("koalas are so cute.\n");
+	printf("Ducks never feel cold, even if they swim in cold water.\n");
+	printf("Platypuses are not very comfortable on land.\n");
+	printf("Bears have a very good sense of smell.\n");
+	printf("Pandas are very special animals.\n");
+	printf("Polar bears look very cute.\n");
+	return 0;
+}
